d6b3b18a0a9259e3f75a2f8445fcbebe509fe3c6
Approximations <datkidbosst@gmail.com>
2023-02-17 13:24:45 -0300

    EJS View Engine

--- express js/template - 02/index.js
+++ express js/template - 02/index.js
@@ -7,9 +7,22 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(function(req, res) {
+//     res.render('index');
+// })
+
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {
+        title: 'Digital Tech - Home'
+    });
 });
+
+app.get('/posts', function (req, res) {
+    res.render('posts', {
+        title: 'Digital Tech - Posts'
+    });
+});
+
 
 app.listen(port, e => {
     console.log('Servidor escutando na porta '+port);
