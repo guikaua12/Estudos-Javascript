7450473d4cc095044406c10ec04de08b3e20617c
Approximations <datkidbosst@gmail.com>
2023-02-20 14:32:59 -0300

    Consertado erro de conexão

--- mongo db/projeto-crud/src/database/index.js
+++ mongo db/projeto-crud/src/database/index.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
 
-const database = {
-    connection: undefined,
-    connect: async function() {
-        mongoose.set('strictQuery', false);
-        this.connection = await mongoose.createConnection('mongodb://127.0.0.1:27017/caralho?tls=false');
-        this.connection.once('open', e => {
-            console.log('Conectado à database.')
-        });
 
-        this.connection.on('error', e => {
-            console.error(e);
-        });
+function connect() {
+    mongoose.set('strictQuery', false);
+    mongoose.connect('mongodb://127.0.0.1:27017/caralho?tls=false');
+    const db = mongoose.connection;
+    db.once('open', e => {
+        console.log('Conectado à database.')
+    });
 
-    }
+    db.on('error', e => {
+        console.error(e);
+    });
 }
 
 
 
-module.exports = database;
+
+module.exports = {
+    connect
+}
--- mongo db/projeto-crud/src/server.js
+++ mongo db/projeto-crud/src/server.js
@@ -2,24 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const router = require('./routes');
-const {connect, connection} = require('./database');
+const database = require('./database');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-connect();
-console.log(connection)
+database.connect();
 
 const schema = new mongoose.Schema({
     name: String,
     age: Number
 });
 
-const Model = mongoose.model('Person', schema);
+const Model = mongoose.model('customers', schema);
 const model = new Model({name: 'Guizao', age: 19})
-model.save().then(d => {
-    console.log(d)
-})
+model.save();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
